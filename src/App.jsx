34c85e17b1461b6bd30ca0cc69f045c085ecd48f
Manish <mdashsharma95@gmail.com>
2024-09-29 06:11:28 +0530

    fixed different url

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import { Button, Container, Box } from '@mui/material';
 import { FaGooglePlay, FaApple } from 'react-icons/fa';
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+const links = {
+  qrcode: {
+    googlePlayLink: "https://play.google.com/store/apps/details?id=com.mycompany.dowellqrcodescannerv3",
+    appleStoreLink: "https://apps.apple.com/us/app/dowell-scanner/id6503245991"
+  },
+  myfridge: {
+    googlePlayLink: "https://play.google.com/store/apps/details?id=com.mycompany.dowellmyfridge",
+    appleStoreLink: "https://apps.apple.com/in/app/dowell-my-fridge/id6670190938"
+  },
+  kiosk: {
+    googlePlayLink: "",
+    appleStoreLink: ""
+  },
+  scale: {
+    googlePlayLink: "https://play.google.com/store/apps/details?id=com.mycompany.dowellqrcodescannerv3",
+    appleStoreLink: "https://apps.apple.com/us/app/dowell-scanner/id6503245991"
+  },
+  other: {
+    googlePlayLink: "https://play.google.com/store/apps/details?id=com.mycompany.dowellqrcodescannerv3",
+    appleStoreLink: "https://apps.apple.com/us/app/dowell-scanner/id6503245991"
+  }
+};
 
 const App = () => {
+  const location = useLocation();
+  const [storeLinks, setStoreLinks] = useState(links.other);
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const productName = queryParams.get("productName") || "other";
+    setStoreLinks(links[productName] || links.other);
+  }, [location]);
+
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-cyan-400 flex justify-center items-center">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-cyan-400">
       <Container maxWidth="sm" className="p-6 bg-white rounded-lg shadow-lg hover:shadow-xl transition-transform transform hover:translate-y-[-5px]">
         <Box textAlign="center" mb={4}>
           <img
@@ -15,20 +49,22 @@ const App = () => {
             <Button
               variant="contained"
               color="primary"
-              href="https://play.google.com/store/apps/details?id=com.mycompany.dowellqrcodescannerv3"
+              href={storeLinks.googlePlayLink || "#"}
               target="_blank"
               className="flex items-center"
               startIcon={<FaGooglePlay />}
+              disabled={!storeLinks.googlePlayLink}
             >
               Google Play
             </Button>
             <Button
               variant="contained"
               color="primary"
-              href="https://apps.apple.com/us/app/dowell-scanner/id6503245991"
+              href={storeLinks.appleStoreLink || "#"}
               target="_blank"
               className="flex items-center"
               startIcon={<FaApple />}
+              disabled={!storeLinks.appleStoreLink}
             >
               App Store
             </Button>
